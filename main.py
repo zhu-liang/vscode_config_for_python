6706dc54a80dddbc2a7b598838b92d03980f9b37
Zhu Liang <zlux2357@gmail.com>
2017-12-04 21:45:44 +0800

    inital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def dummy_func():
+    print('hello world')
+    friends = ['alice', 'bob', 'charlie', 'dick']
+    for i in friends:
+        print("hello {0}".format(i))
+
+if __name__ == "__main__":
+    dummy_func()
+
